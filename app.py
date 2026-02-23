140f17b6103b6997833072dd87e4e79d8190c74d
DonatasNoreika <donoras@gmail.com>
2026-02-23 18:37:26 +0200

    +Sukurti puslapį, kuriame galima būtų įvesti pajamas arba išlaidas (su - ženklu), peržiūrėti juos bei paskaičiuotą visų sumų balansą.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from calendar import isleap
 
 app = Flask(__name__)
+
+zurnalas = []
 
 @app.route("/")
 def index():
@@ -13,6 +15,15 @@ def keliamieji():
         metai = int(request.form['metai'])
         return render_template("rezultatas.html", metai=metai, isleap=isleap)
     return render_template("keliamieji.html")
+
+
+@app.route("/biudzetas/", methods=["GET", "POST"])
+def biudzetas():
+    if request.method == "POST":
+        suma = int(request.form['suma'])
+        zurnalas.append(suma)
+    balansas = sum(zurnalas)
+    return render_template("biudzetas.html", zurnalas=zurnalas, balansas=balansas)
 
 if __name__ == "__main__":
     app.run(debug=True)
